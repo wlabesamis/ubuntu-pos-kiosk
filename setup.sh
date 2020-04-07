f5d74988ec6b6d188acccaadf5adbf908c4a1d62
Wilson Abesamis <wabesamis@usautoparts.com>
2020-04-07 22:27:45 +0800

    added timeout and remove auto login in tty1

--- setup.sh
+++ setup.sh
@@ -13,14 +13,6 @@ echo "needs_root_rights=yes" | sudo tee -a /etc/X11/Xwrapper.config
 #Enable tty1 tty2 tty3
 sudo systemctl enable getty@tty1 getty@tty2 getty@tty3
 
-#Enable auto login in tty1
-sudo install -b -m 644 /dev/stdin /etc/systemd/system/getty\@tty1.service.d/override.conf << EOF
-[Service]
-ExecStart=
-ExexStart=-/sbin/agetty --autologin kiosk --noclear %I $TERM
-Type=idle
-EOF
-
 #install kiosh service
 sudo cp kiosk.sh /opt/
 sudo chmod +x /opt/kiosk.sh
